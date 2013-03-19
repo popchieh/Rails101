932bbdf04b6933aee908e5e77bfef919ba027b16
Evan <popchieh07@gmail.com>
2013-03-19 11:26:57 +0800

    scaffold finished

---
+++ forum_demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForumDemo::Application.config.secret_token = 'b21e43ca552640692986b1ef82ca616e42f0950f00218d40b38f0070d511f38fe7d66ac238633e6ed8bd2382277b4719666d7280fdb8cab2d910545032262aec'
