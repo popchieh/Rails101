35ace01274ce310ff4584e63fa62eb4f31eff84e
Evan <popchieh07@gmail.com>
2013-04-09 19:49:35 +0800

    ch9 finished

--- forum_demo/app/models/post.rb
+++ forum_demo/app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :content, :title
+  attr_accessible :content, :title, :excerpt_image
   belongs_to :board, :counter_cache => true
   #default_scope :order => 'created_at DESC'
   scope :recent, :order => 'updated_at DESC'
+  has_attached_file :excerpt_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
