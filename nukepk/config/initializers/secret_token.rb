178ee977e06026feacb4c20bfaa18fec32e9e42a
Evan <popchieh07@gmail.com>
2013-05-11 11:16:51 +0800

    use bootstrappers to build nukepk

---
+++ nukepk/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nukepk::Application.config.secret_token = '05efea6024c0d04049c40f2a7a9f0703c6f38da37f204d6a4fd27d889e8cea8a92f00c90b26078afd29ebda0efd0c57afe28187bbbcb07635234b0187bbefd08'
