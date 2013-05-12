2b4472272b2e1ef9665c93a7ac72ac609cedbb35
Evan <popchieh07@gmail.com>
2013-05-12 17:20:10 +0800

    nukepk-for check

--- nukepk/config/routes.rb
+++ nukepk/config/routes.rb
@@ -1,6 +1,17 @@
 Nukepk::Application.routes.draw do
+  devise_for :users
+
+  resources :posts
+
+
+  resources :boards do
+  	resources :posts
+  end
+
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   root :to => 'high_voltage/pages#show', :id => 'welcome'
 
 end
+
