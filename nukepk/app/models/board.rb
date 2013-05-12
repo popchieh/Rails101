b1d7d8ae3f0940be9782a62747248b26ebeaade0
Evan <popchieh07@gmail.com>
2013-05-12 17:20:33 +0800

    Revert "nukepk-for check"
    This reverts commit 2b4472272b2e1ef9665c93a7ac72ac609cedbb35.

--- nukepk/app/models/board.rb
+++
@@ -1,4 +0,0 @@
-class Board < ActiveRecord::Base
-  # attr_accessible :name
-  has_many :posts
-end
--- nukepk/app/models/post.rb
+++
@@ -1,4 +0,0 @@
-class Post < ActiveRecord::Base
-  # attr_accessible :content, :title
-  belongs_to :board
-end
